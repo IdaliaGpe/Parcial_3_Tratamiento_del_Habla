f50f885d8d5bb54ba361eddc6ebb1a0dee467d03
IdaliaGpe <idaliaguadalupepucca@hotmail.com>
2022-04-04 09:37:37 -0700

    04/04/22

--- microfono_stream.py
+++ microfono_stream.py
@@ -1,2 +1,13 @@
 import sounddevice as sd
 import numpy as np
+
+print(sd.query_devices())
+#1 entrada
+#3 salida
+
+try: #Intenta hacer esto
+    with sd.Stream():
+        print('Presiona tecla Enter para salir')
+        input()
+except Exception as e: #Si fallas has esto
+    print(str(e))
